ad6f8a639a393c47027c2f35031f1c8e37ee2bcc
Maksym <bienkowski.maksym@gmail.com>
2025-11-12 12:01:32 +0000

    feat: pass camera view & projection, draw cube

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,16 @@
 use glium::backend::glutin::SimpleWindowBuilder;
+use glm::Mat4;
 use winit::{
     application::ApplicationHandler, event::WindowEvent, event_loop::ActiveEventLoop,
     window::WindowId,
 };
 
-use crate::renderer::Renderer;
+use crate::{camera::FlyCamera, renderer::Renderer};
 
 #[derive(Default)]
 pub struct App {
     renderer: Option<Renderer>,
+    camera: Option<FlyCamera>,
 }
 
 impl ApplicationHandler for App {
@@ -18,6 +20,10 @@ impl ApplicationHandler for App {
             .build(event_loop);
 
         self.renderer = Some(Renderer::new(window, display));
+        self.camera = Some(FlyCamera::new(
+            glm::vec3(0.0, 0.0, 5.0),
+            self.renderer.as_ref().unwrap().get_aspect_ratio(),
+        ));
     }
 
     fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
@@ -41,7 +47,15 @@ impl ApplicationHandler for App {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::RedrawRequested => {
                 if let Some(renderer) = &self.renderer {
-                    renderer.draw_cube();
+                    let model = Mat4::identity();
+                    let model = glm::rotate_x(&model, 45.0f32.to_radians());
+                    let model = glm::rotate_y(&model, 45.0f32.to_radians());
+
+                    renderer.draw_cube(
+                        model.into(),
+                        self.camera.as_ref().unwrap().get_view_matrix(),
+                        self.camera.as_ref().unwrap().get_projection_matrix(),
+                    );
                 }
             }
             _ => {}
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,5 +1,9 @@
+use crate::shaders::{FRAGMENT_SHADER_SRC, VERTEX_SHADER_SRC};
 use glium::glutin::surface::WindowSurface;
-use glium::{Display, DrawParameters, Program, Surface, implement_vertex};
+use glium::{
+    Depth, DepthTest, Display, DrawParameters, Program, Surface, implement_vertex, uniform,
+};
+use glm::Mat3;
 use winit::window::Window;
 
 pub struct Renderer {
@@ -20,28 +24,31 @@ impl Renderer {
         self.display.resize(new_size);
     }
 
-    pub fn draw(&self, vertices: &Vec<Vertex>, indices: &Vec<u16>) {
+    pub fn draw(
+        &self,
+        vertices: &[Vertex],
+        indices: &[u16],
+        model_matrix: [[f32; 4]; 4],
+        view_matrix: [[f32; 4]; 4],
+        projection_matrix: [[f32; 4]; 4],
+    ) {
         let mut frame = self.display.draw();
-        frame.clear_color(1.0, 0.0, 0.0, 1.0);
-        let program = Program::from_source(
-            &self.display,
-            "
-            #version 140
-            in vec3 position;
-            in vec3 normal;
-            void main() {
-                gl_Position = vec4(position, 1.0);
-            }
-            ",
-            "
-            #version 140
-            out vec4 color;
-            void main() {
-                color = vec4(0.0, 1.0, 0.0, 1.0);
-            }
-            ",
-            None,
-        );
+        frame.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+        let program =
+            Program::from_source(&self.display, VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC, None);
+
+        let model_mat3 = Mat3::from_fn(|r, c| model_matrix[r][c]);
+        let normal_matrix: [[f32; 3]; 3] = glm::inverse_transpose(model_mat3).into();
+
+        let params = DrawParameters {
+            depth: Depth {
+                test: DepthTest::IfLess,
+                write: true,
+                ..Depth::default()
+            },
+            ..DrawParameters::default()
+        };
+
         frame
             .draw(
                 &glium::VertexBuffer::new(&self.display, vertices).unwrap(),
@@ -52,15 +59,22 @@ impl Renderer {
                 )
                 .unwrap(),
                 &program.unwrap(),
-                &glium::uniforms::EmptyUniforms,
-                &DrawParameters::default(),
+                &uniform! {model: model_matrix, view: view_matrix, projection: projection_matrix, normal_matrix: normal_matrix},
+                &params,
             )
             .expect("Failed to draw frame");
         frame.finish().expect("Failed to destroy frame");
     }
 
-    pub fn draw_cube(&self) {
+    #[allow(clippy::too_many_lines)]
+    pub fn draw_cube(
+        &self,
+        model_matrix: [[f32; 4]; 4],
+        view_matrix: [[f32; 4]; 4],
+        projection_matrix: [[f32; 4]; 4],
+    ) {
         let vertices = vec![
+            // Back face (z = -0.5)
             Vertex {
                 position: [-0.5, -0.5, -0.5],
                 normal: [0.0, 0.0, -1.0],
@@ -77,6 +91,7 @@ impl Renderer {
                 position: [-0.5, 0.5, -0.5],
                 normal: [0.0, 0.0, -1.0],
             },
+            // Front face (z = 0.5)
             Vertex {
                 position: [-0.5, -0.5, 0.5],
                 normal: [0.0, 0.0, 1.0],
@@ -93,14 +108,98 @@ impl Renderer {
                 position: [-0.5, 0.5, 0.5],
                 normal: [0.0, 0.0, 1.0],
             },
+            // Left face (x = -0.5)
+            Vertex {
+                position: [-0.5, -0.5, -0.5],
+                normal: [-1.0, 0.0, 0.0],
+            },
+            Vertex {
+                position: [-0.5, -0.5, 0.5],
+                normal: [-1.0, 0.0, 0.0],
+            },
+            Vertex {
+                position: [-0.5, 0.5, 0.5],
+                normal: [-1.0, 0.0, 0.0],
+            },
+            Vertex {
+                position: [-0.5, 0.5, -0.5],
+                normal: [-1.0, 0.0, 0.0],
+            },
+            // Right face (x = 0.5)
+            Vertex {
+                position: [0.5, -0.5, -0.5],
+                normal: [1.0, 0.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, -0.5, 0.5],
+                normal: [1.0, 0.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, 0.5, 0.5],
+                normal: [1.0, 0.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, 0.5, -0.5],
+                normal: [1.0, 0.0, 0.0],
+            },
+            // Bottom face (y = -0.5)
+            Vertex {
+                position: [-0.5, -0.5, -0.5],
+                normal: [0.0, -1.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, -0.5, -0.5],
+                normal: [0.0, -1.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, -0.5, 0.5],
+                normal: [0.0, -1.0, 0.0],
+            },
+            Vertex {
+                position: [-0.5, -0.5, 0.5],
+                normal: [0.0, -1.0, 0.0],
+            },
+            // Top face (y = 0.5)
+            Vertex {
+                position: [-0.5, 0.5, -0.5],
+                normal: [0.0, 1.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, 0.5, -0.5],
+                normal: [0.0, 1.0, 0.0],
+            },
+            Vertex {
+                position: [0.5, 0.5, 0.5],
+                normal: [0.0, 1.0, 0.0],
+            },
+            Vertex {
+                position: [-0.5, 0.5, 0.5],
+                normal: [0.0, 1.0, 0.0],
+            },
         ];
 
-        let indices = vec![
-            0u16, 1, 2, 2, 3, 0, 4u16, 5, 6, 6, 7, 4, 0u16, 4, 7, 7, 3, 0, 1u16, 5, 6, 6, 2, 1,
-            3u16, 2, 6, 6, 7, 3, 0u16, 1, 5, 5, 4, 0,
+        let indices: Vec<u16> = vec![
+            0, 1, 2, 0, 2, 3, // Back
+            4, 5, 6, 4, 6, 7, // Front
+            8, 9, 10, 8, 10, 11, // Left
+            12, 13, 14, 12, 14, 15, // Right
+            16, 17, 18, 16, 18, 19, // Bottom
+            20, 21, 22, 20, 22, 23, // Top
         ];
 
-        self.draw(&vertices, &indices);
+        self.draw(
+            &vertices,
+            &indices,
+            model_matrix,
+            view_matrix,
+            projection_matrix,
+        );
+    }
+
+    #[allow(clippy::cast_precision_loss)]
+    pub fn get_aspect_ratio(&self) -> f32 {
+        let size = self.window.inner_size();
+        size.width as f32 / size.height as f32
     }
 }
 
